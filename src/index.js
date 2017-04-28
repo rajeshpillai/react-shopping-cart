74c19a0b1c2d771c417472f0f5943faa67ef7c94
Rajesh Pillai <pillai.rajesh@gmail.com>
2017-04-28 11:45:17 +0530

    Added reducers basic

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import App from './App';
 import reducers from './reducers';
 import './index.css';
 
-const store = createStore(reducers);
+const store = createStore(reducers, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 
 // Provider gives access to redux store via connect()
 ReactDOM.render(
