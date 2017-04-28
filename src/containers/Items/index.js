753828b7a8a4ddfaffec50512bc7076fc520892c
Rajesh Pillai <pillai.rajesh@gmail.com>
2017-04-28 12:21:59 +0530

    Implementing add to cart and connect to action

--- src/containers/Items/index.js
+++ src/containers/Items/index.js
@@ -1,14 +1,15 @@
 import React, {Component} from 'react';
 import { connect } from 'react-redux';
+import { addToCart } from '../../actions';
 import './items.css';
 
 export class Items extends Component {
   renderList= () => {
-    const { items } = this.props;
+    const { items, addToCartAction } = this.props;
     return items.map((item) => (
       <li key={item.id}
           className={'Items-list-item'}
-          onClick={() => console.log('added to store')}
+          onClick={() => addToCartAction(item) }
       >
         <img role="presentation" src={item.link} />
         <span>{item.title}</span>
@@ -28,4 +29,8 @@ const mapStateToProps = (reduxState) => ({
   items: reduxState.items
 });
 
-export default connect(mapStateToProps)(Items);
+const mapDispatchToProps = (dispatch) => ({
+  addToCartAction: (item) => dispatch(addToCart(item))
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(Items);
--- src/reducers/shoppingCart.js
+++ src/reducers/shoppingCart.js
@@ -1,7 +1,11 @@
+import { ADD_TO_CART } from '../actions';
+
 const INIT_STATE = [];
 
 export default (state = INIT_STATE, action) => {
   switch (action.type) {
+    case ADD_TO_CART:
+      return [...state, action.payload];
     default:
       return state
   }
