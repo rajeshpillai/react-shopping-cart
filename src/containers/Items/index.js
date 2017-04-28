0a5e214bc072ffd51cad8703c3ed5b02e0be070d
Rajesh Pillai <pillai.rajesh@gmail.com>
2017-04-28 12:57:35 +0530

    Added styling

--- src/containers/Items/index.js
+++ src/containers/Items/index.js
@@ -11,7 +11,7 @@ export class Items extends Component {
           className={'Items-list-item'}
           onClick={() => addToCartAction(item) }
       >
-        <img role="presentation" src={item.link} />
+        <img role="presentation" className={'Items-image'} src={item.link} />
         <span>{item.title}</span>
       </li>
     ))
